6494d8c503fee4cf3622ff5bdd4f3d98536b7c58
DeyMoinak <73658041+DeyMoinak@users.noreply.github.com>
2024-01-22 18:43:34 -0500

    Update index.js

--- src/Components/Membership/index.js
+++ src/Components/Membership/index.js
@@ -9,7 +9,7 @@ const Membership = () => {
             <h1 class="memb_title">Become A Member</h1>
 
             <section class="membership">
-                <body>Discord: https://discord.gg/XgZyxRd8</body>
+                <body>Discord: https://discord.gg/cRYjCuFb</body>
                 <body>Instagram: https://www.instagram.com/dsc.uf/</body>
             </section>
         </body>
